9e96acdf3ce8edd02fa3dc05c72b76d126d1de39
iranmzk <iranmpc93@gmail.com>
2022-08-22 10:22:30 -0300

    Swagger funcionando

--- src/main/java/demo/sprint/configuration/swaggerconfig/SpringFoxConfig.java
+++ src/main/java/demo/sprint/configuration/swaggerconfig/SpringFoxConfig.java
@@ -2,12 +2,18 @@ package demo.sprint.configuration.swaggerconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
+@EnableSwagger2
+@EnableWebMvc
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
@@ -15,6 +21,18 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo info(){
+        return new ApiInfoBuilder()
+                .title("Walmart and Amazon API")
+                .contact(new Contact("Iran",
+                        "https://www.linkedin.com/in/iran-mizunski-5824b416b/",
+                        "testeApi@gmail.com"))
+                .description("Consulte prices and stock in the Walmart and Amazon shop")
+                .version("Beta")
+                .license("terms of service")
                 .build();
     }
 }
